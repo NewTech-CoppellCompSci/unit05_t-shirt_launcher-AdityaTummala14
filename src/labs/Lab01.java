78841ea9d2b487bb279c037d9d2bce23f120e766
AdityaTummala14 <113065533+AdityaTummala14@users.noreply.github.com>
2023-01-06 16:08:48 -0600

    Lab 1, 2

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -18,31 +18,56 @@ public class Lab01 {
 	
 	
 	public static void problem1() {
-	
-		
+	Scanner inKey = new Scanner(System.in);
+	System.out.print("Enter a positive integer: ");
+	int input = inKey.nextInt();
+	System.out.print("Enter another positive integer: ");
+	int input2 = inKey.nextInt();
+	System.out.println(input + "^" + input2 + " = " + Math.pow(input, input2));
 	}
 	
 	
 	
 	public static void problem2() {
-		
-			
+	Scanner inKey = new Scanner(System.in);
+	System.out.print("Enter a positive number: ");
+	int input = inKey.nextInt();
+	System.out.println("The square root of "+ input + " is " + Math.sqrt(input));
 	}
 	
 	
 	
 	public static void problem3() {
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Length of side A: ");
+		int input = inKey.nextInt();
+		System.out.print("Length of side B: ");
+		int input2 = inKey.nextInt();
+		double sa = Math.pow(input, 2);
+		double sb = Math.pow(input2, 2);
+		System.out.println("Hypotenuse = " + Math.sqrt(sa+sb) );
 		
 	}
 	
 	
 	
 	public static void problem4() {
-		
-		
-	}
+		Scanner inKey = new Scanner(System.in);
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int input = 1;
+		while(input != 0) {
+			
+			System.out.println("Enter an integer: ");
+			input = inKey.nextInt();
+			max = Math.max(max, input);
+			min = Math.min(min, input);
+		}
+		System.out.println("Max = "+ max);
+		System.out.println("Min = "+ min);
 	
 	
 	
+	
+}
 }
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -15,27 +15,63 @@ public class Lab02 {
 
 
 	public static void problem1() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a positive integer: ");
+		int input = inKey.nextInt();
+		System.out.print("Enter another positive integer: ");
+		int input2 = inKey.nextInt();
+		int max = Math.max(input, input2);
+		int min = Math.min(input, input2);
+		for (int i = 0; i<100; i++) {
+			System.out.print((int) (Math.random()*(max - min + 1)) + min + " ");
+		}
+		System.out.print("\n");
 	}
 
 
 	public static void problem2() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("What is the radius of your cylinder: ");
+		double rad = inKey.nextDouble();
+		System.out.print("What is the height of your cylinder: ");
+		double ht = inKey.nextDouble();
+		System.out.print(Math.PI*(rad*rad)*ht);
+		System.out.print("\n");
 	}
 
 
 	public static void problem3() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter x1: ");
+		double x1 = inKey.nextDouble();
+		System.out.print("Enter y1: ");
+		double y1 = inKey.nextDouble();
+		System.out.print("Enter x2: ");
+		double x2 = inKey.nextDouble();
+		System.out.print("Enter y2: ");
+		double y2 = inKey.nextDouble();
+		double x = Math.pow((x2-x1),2);
+		double y = Math.pow((y2-y1),2);
+		double xy = x + y;
+		System.out.print("Distance between points = " + Math.sqrt(xy));
 	}
 
 	
 	
 	public static void problem4() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a-value: ");
+		double a = inKey.nextDouble();
+		System.out.print("Enter b-value: ");
+		double b = inKey.nextDouble();
+		System.out.print("Enter c-value: ");
+		double c = inKey.nextDouble();
+		double p1p = b*(-1) + (Math.sqrt(b*b - 4*a*c));
+		double p2p = p1p/(2*a);
+		double p1n = b*(-1) - (Math.sqrt(b*b - 4*a*c));
+		double p2n = p1n/(2*a);
+		System.out.println("x1 = " + p2p);
+		System.out.println("x2 = " + p2n);
 	}
 
 
